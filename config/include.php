947fe5dd322ca3269db1c90c950c9ed8d145b356
ismail <ugurdrky@gmail.com>
2023-12-07 03:02:01 +0300

    anasayfa düzenlendi, giriş yap, kayıt ol sayfaları oluşturuldu.

--- config/include.php
+++ config/include.php
@@ -15,6 +15,7 @@ require_once __DIR__ . "/../controller/pages/marka.php";
 require_once __DIR__ . "/../controller/pages/marka-detay.php";
 require_once __DIR__ . "/../controller/pages/kategori.php";
 require_once __DIR__ . "/../controller/pages/kategoridetay.php";
+require_once __DIR__ . "/../controller/pages/girisyap.php";
 $ROUTE = new engine([
     'views' => __DIR__ . '/../view/finview',
     'cache' => __DIR__ . '/../storage/cache/finview',
